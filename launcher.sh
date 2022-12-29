dbcb60761775e05671eef34e92d801a4245f3f13
Tigur <SirResnick2@gmail.com>
2022-12-29 22:03:48 +0100

    first commit

---
+++ launcher.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# laucher.sh
+
+cd /
+cd home/camera/GUI/
+#sudo python rpi_scanner_OK.py
+#lxterminal -e "sudo python /home/camera/GUI/rpi_scanner_OK.py"
+lxterminal -e "sudo python /home/camera/GUI/Hello.py"
+cd / 
---
+++ old_Versions/fileTra.py
@@ -0,0 +1,20 @@
+import os
+
+
+
+master_ip = "192.168.17.234"
+
+sciezka = "/home/camera/Pictures/"
+path_master = "/home/projekt1/Desktop/Pliki/PB/"
+haslo = "haslo123"
+pliki_str = ""
+
+for path in os.listdir(sciezka):
+    filename = os.path.join(sciezka,path)
+    
+    if os.path.isfile(filename):
+        pliki_str = pliki_str + filename +" "
+print(pliki_str )
+os.system("sshpass -p "+haslo+" scp "+pliki_str+"camera@"+master_ip+":"+path_master)
+#os.system("sshpass -p -o StrictHostKeyChecking=no "+haslo+" scp "+pliki_str+"projekt1@"+master_ip+":"+path_master)
+#os.system("rm -f "+sciezka+"*")
